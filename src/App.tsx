6275d1566925ac90e5bb6c363adde1d2dccb72f4
ABojo <Alex.bojorquez2598@gmail.com>
2023-08-10 13:28:45 -0400

    Setup routing in App
    Installed react-router-dom to handle the client side routing. Setup a route for
    the home page and a route for viewing a specific planet. I added placeholder elements
    to be rendered for the time being.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import planetData from "./utils/data.json";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [planets, setPlanets] = useState(planetData);
 
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/:planet" element={<div>PLanet</div>} />
+          <Route index element={<div>HOME</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
