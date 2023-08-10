91884e7b7196d1fec66888580fcc6fddc8b34bdd
ABojo <Alex.bojorquez2598@gmail.com>
2023-08-10 16:54:48 -0400

    Added the Header Component to the App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import planetData from "./utils/data.json";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   const [planets, setPlanets] = useState(planetData);
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header planets={planets} />
         <Routes>
           <Route path="/:planet" element={<div>PLanet</div>} />
           <Route index element={<div>HOME</div>} />
