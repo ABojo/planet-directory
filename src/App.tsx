8dca31542288ff1c154320d25f7acd281e8ec1a1
ABojo <Alex.bojorquez2598@gmail.com>
2023-08-11 21:17:53 -0400

    Added planet state, findPlanetByName function, and fixed planet path
    I added planet state that will hold the planet JSON data. I created a
    findPlanetByName function that will be used to find planets when they're queried.
    I forgot to add the '/planet' to the route for viewing planets so I addded that
    in.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import { useState } from "react";
 import planetData from "./utils/data.json";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Planet from "./components/Planet/Planet";
+import PlanetT from "./types/Planet";
 
 function App() {
-  const [planets, setPlanets] = useState(planetData);
+  const [planets] = useState(planetData);
+
+  function findPlanetByName(planetName?: string): PlanetT | undefined {
+    if (!planetName) return;
+
+    const pNameLowerCase = planetName.toLowerCase();
+
+    return planets.find((planet) => planet.name.toLowerCase() == pNameLowerCase);
+  }
 
   return (
     <div>
       <BrowserRouter>
         <Header planets={planets} />
         <Routes>
-          <Route path="/:planet" element={<div>PLanet</div>} />
+          <Route path="/planet/:planet" element={<Planet findPlanetByName={findPlanetByName} />} />
           <Route index element={<div>HOME</div>} />
         </Routes>
       </BrowserRouter>
